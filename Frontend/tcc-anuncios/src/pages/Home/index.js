fae74a8f5eb7f53efc74f61e80d23467ba8e5cbf
matheus1355 <matheus.4002@hotmail.com>
2020-10-22 19:39:25 -0300

    Ajustes tela home

--- Frontend/tcc-anuncios/src/pages/Home/index.js
+++ Frontend/tcc-anuncios/src/pages/Home/index.js
@@ -111,6 +111,7 @@ export default function Home(props){
                         </div>
                     </div> 
                 </div>
+
             </div>
             
             <div className="rodape">
